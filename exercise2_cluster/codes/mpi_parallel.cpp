4adffe756aa30712cd82e87e9dd77b426644e1b3
Tommaso Ronconi <tommaso.ronconi@outlook.it>
2021-12-05 18:56:11 +0100

    first commit

---
+++ exercise2_cluster/codes/mpi_parallel.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <mpi.h>
+
+int main ( int argc, char * argv[] ) {
+
+  int rank, npes;
+
+  MPI_Init(argc, argv);
+  MPI_Comm_size( MPI_COMM_WORLD, &npes );
+  MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+
+  std::cout <<
+    "Hello from rank " +
+    std::to_string( rank ) + " of " +
+    std::to_string( npes ) + "\n";
+  
+  MPI_Finalize();
+
+  return 0;
+
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# The Hitchhiker's guide to not screw up
+
+Here you will find the exercises of the course.
